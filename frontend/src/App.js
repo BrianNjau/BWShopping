dd265584687e998770884c12e051b763da7fbd36
Brian Njau <brian.njau@strathmore.edu>
2021-02-04 17:12:38 +0300

    Cleaning Up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
-
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Homescreen from "./screens/Homescreen";
+import Productscreen from "./screens/Productscreen";
+import Cartscreen from "./screens/Cartscreen";
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to BW_Shop</h1>
-    </div>
+    <Router>
+      <Header />
+
+      <main className="py-3">
+        <Container>
+          <Route path="/" component={Homescreen} exact />
+          <Route path="/product/:id" component={Productscreen} />
+          <Route path="/cart/:id?" component={Cartscreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 
